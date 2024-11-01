e4ea36f397d314d2b3e981235f4da4edfc482e36
Stephane Joly <stephane.joly1965@gmail.com>
2024-11-01 17:04:19 -0500

    Add files via upload

---
+++ AIJokeGenerator/ai-joke-app/app/page.tsx
@@ -0,0 +1,312 @@
+// import Image from "next/image";
+
+// export default function Home() {
+//   return (
+//     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+//       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+//         <Image
+//           className="dark:invert"
+//           src="/next.svg"
+//           alt="Next.js logo"
+//           width={180}
+//           height={38}
+//           priority
+//         />
+//         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+//           <li className="mb-2">
+//             Get started by editing{" "}
+//             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+//               app/page.tsx
+//             </code>
+//             .
+//           </li>
+//           <li>Save and see your changes instantly.</li>
+//         </ol>
+
+//         <div className="flex gap-4 items-center flex-col sm:flex-row">
+//           <a
+//             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+//             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             <Image
+//               className="dark:invert"
+//               src="/vercel.svg"
+//               alt="Vercel logomark"
+//               width={20}
+//               height={20}
+//             />
+//             Deploy now
+//           </a>
+//           <a
+//             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+//             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Read our docs
+//           </a>
+//         </div>
+//       </main>
+//       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+//         <a
+//           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+//           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <Image
+//             aria-hidden
+//             src="/file.svg"
+//             alt="File icon"
+//             width={16}
+//             height={16}
+//           />
+//           Learn
+//         </a>
+//         <a
+//           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+//           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <Image
+//             aria-hidden
+//             src="/window.svg"
+//             alt="Window icon"
+//             width={16}
+//             height={16}
+//           />
+//           Examples
+//         </a>
+//         <a
+//           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+//           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <Image
+//             aria-hidden
+//             src="/globe.svg"
+//             alt="Globe icon"
+//             width={16}
+//             height={16}
+//           />
+//           Go to nextjs.org →
+//         </a>
+//       </footer>
+//     </div>
+//   );
+// }
+
+// new code
+"use client";
+
+import { useState } from "react";
+import { useChat } from "ai/react";
+
+export default function Chat() {
+  const { messages, append, isLoading } = useChat();
+
+  const topics = [
+    { value: "Work" },
+    { value: "People" },
+    { value: "Animals" },
+    { value: "Food" },
+    { value: "Television" },
+  ];
+
+  const tones = [
+    { value: "Witty" },
+    { value: "Sarcastic" },
+    { value: "Silly" },
+    { value: "Dark" },
+    { value: "Goofy" },
+  ];
+
+  const types = [
+    { value: "Pun" },
+    { value: "Knock-Knock" },
+    { value: "Story" },
+    { value: "One-line" },
+    { value: "Satire" },
+  ];
+
+  const temperature = [ 
+    {value: "0.0"},
+    {value: "0.1"},
+    {value: "0.2"},
+    {value: "0.3"},
+    {value: "0.4"},
+    {value: "0.5"},
+    {value: "0.6"},
+    {value: "0.7"},
+    {value: "0.8"},
+    {value: "0.9"},
+    {value: "1.0"},
+  ];
+
+  const subjective = [
+    {value: "Funny"},
+    {value: "Appropriate"},
+    {value: "Offensive"},
+    {value: "Creative"},
+    {value: "Original"},
+  ];
+
+  const [state, setState] = useState({
+    topics: "",
+    tones: "",
+    types: "",
+    temperatures: "",
+  });
+  
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  return (
+    <main className="mx-auto w-full p-24 flex flex-col">
+      <div className="p4 m-4">
+        <div className="flex flex-col items-center justify-center space-y-8 text-white">
+          <div className="space-y-2">
+            <h2 className="text-3xl font-bold">Story Telling App</h2>
+            <p className="text-zinc-500 dark:text-zinc-400">
+              Customize a joke by selecting the genre, tone, type and how creative OpenAI can be.
+            </p>
+          </div>
+
+          {/* TOPICS */}
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Topics</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {topics.map(({value}) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    value={value}
+                    name="topic"
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* TONES */}
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Tones</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {tones.map(({value}) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    name="tone"
+                    value={value}
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* TYPES */}
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Types</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {types.map(({value}) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    name="type"
+                    value={value}
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* TEMPERATURE */}
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Temperatures</h3>
+
+            <div className="flex flex-wrap justify-center">
+              {temperature.map(({value}) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    name="temps"
+                    value={value}
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* BUTTON */}
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
+            disabled={isLoading || !state.topics || !state.tones || !state.types || !state.temperatures}
+            onClick={() =>
+              append({
+                role: "user",
+                content: `Generate a ${state.topics} joke in a ${state.tones} tone, using the ${state.types} type of humor and ${state.temperatures} degree to which is said.`,
+              })
+            }
+          >
+            Generate Story
+          </button>
+
+          <div
+            hidden={
+              messages.length === 0 ||
+              messages[messages.length - 1]?.content.startsWith("Generate")
+            }
+            className="bg-opacity-25 bg-gray-700 rounded-lg p-4"
+          >
+            {messages[messages.length - 1]?.content}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
